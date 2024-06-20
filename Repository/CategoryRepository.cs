d07c38ae209b677e18c9890920c18af35897ecb5
Lucas Rodrigues <lucas0rodrigues10@gmail.com>
2024-06-20 19:11:44 -0300

    feat

--- Repository/CategoryRepository.cs
+++ Repository/CategoryRepository.cs
@@ -17,8 +17,6 @@ namespace apicsharp.Repository
         {
             _context.Add(category);
             _context.SaveChanges();
-
-            
             
             return Get(category.Id);
         }
